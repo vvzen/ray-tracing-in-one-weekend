21c7ba3b757e1b0aaae0afcd26e49a9037677f25
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2020-03-01 17:05:10 +0000

    Add ch06

---
+++ chapter_06/random.h
@@ -0,0 +1,18 @@
+#ifndef RANDOMH
+#define RANDOMH
+
+#include <functional>
+#include <random>
+
+#include <cstdlib>
+
+inline double random_double(){
+    // static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    // static std::mt19937 generator;
+    // static std::function<double()> rand_generator = std::bind(distribution, generator);
+    // return rand_generator();
+
+    return rand() / (RAND_MAX + 1.0);
+}
+
+#endif
