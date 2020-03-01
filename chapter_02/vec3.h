b218f759624c3a31045b70dfd0f359ecec80df71
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2020-03-01 11:45:22 +0000

    fix vector operator overloads, return address instead of value

--- chapter_02/vec3.h
+++ chapter_02/vec3.h
@@ -47,14 +47,14 @@ class vec3 {
     }
 
     // Vector operations
-    inline vec3 operator+=(const vec3 &v2);
-    inline vec3 operator-=(const vec3 &v2);
-    inline vec3 operator*=(const vec3 &v2);
-    inline vec3 operator/=(const vec3 &v2);
+    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator-=(const vec3 &v2);
+    inline vec3& operator*=(const vec3 &v2);
+    inline vec3& operator/=(const vec3 &v2);
 
     // Scalar operations
-    inline vec3 operator*=(const float t);
-    inline vec3 operator/=(const float t);
+    inline vec3& operator*=(const float t);
+    inline vec3& operator/=(const float t);
 
     inline float length() {
         return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
@@ -68,21 +68,19 @@ class vec3 {
 
 };
 
-inline std::istream& operator>>(std::istream &is, vec3 &v){
-    is >> v.e[0] >> v.e[1] >> v.e[2];
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
     return is;
 }
 
-inline std::ostream& operator>>(std::ostream &os, vec3 &v){
-    os << v.e[0] << " " << v.e[1] << " " << v.e[2];
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
     return os;
 }
 
-inline void vec3::make_unit_vector(){
-    float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
-    e[0] *= k;
-    e[1] *= k;
-    e[2] *= k;
+inline void vec3::make_unit_vector() {
+    float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
@@ -97,77 +95,75 @@ inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v1, const float t) {
-    return vec3(t * v1.e[0], t * v1.e[1], t * v1.e[2]);
+inline vec3 operator*(float t, const vec3 &v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const float t, const vec3 &v1) {
-    return vec3(t * v1.e[0], t * v1.e[1], t * v1.e[2]);
+inline vec3 operator*(const vec3 &v, float t) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
-inline vec3 operator/(const vec3 &v1, const float t) {
-    return vec3(t / v1.e[0], t / v1.e[1], t / v1.e[2]);
+inline vec3 operator/(vec3 v, float t) {
+    return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
 }
 
-inline vec3 operator/(const float t, const vec3 &v1) {
-    return vec3(t / v1.e[0], t / v1.e[1], t / v1.e[2]);
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.e[0]*v2.e[0]
+         + v1.e[1]*v2.e[1]
+         + v1.e[2]*v2.e[2];
 }
 
-// Vector operations
-inline float dot(const vec3 &v1, const vec3 &v2){
-    return v1.e[0] * v2.e[0]
-         + v1.e[1] * v2.e[1]
-         + v1.e[2] * v2.e[2];
-}
-
-inline vec3 cross(const vec3 &v1, const vec3 &v2){
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
                 v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2],
                 v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
 }
 
-// Element wise operations
-inline vec3 vec3::operator+=(const vec3 &v2){
-    return vec3(this->e[0] += v2.e[0],
-                this->e[1] += v2.e[1],
-                this->e[2] += v2.e[2]);
-}
-
-inline vec3 vec3::operator-=(const vec3 &v2){
-    return vec3(this->e[0] -= v2.e[0],
-                this->e[1] -= v2.e[1],
-                this->e[2] -= v2.e[2]);
-}
-
-inline vec3 vec3::operator*=(const vec3 &v2){
-    return vec3(this->e[0] *= v2.e[0],
-                this->e[1] *= v2.e[1],
-                this->e[2] *= v2.e[2]);
-}
-
-inline vec3 vec3::operator/=(const vec3 &v2){
-    return vec3(this->e[0] /= v2.e[0],
-                this->e[1] /= v2.e[1],
-                this->e[2] /= v2.e[2]);
-}
-
-inline vec3 vec3::operator*=(const float t){
-    this->e[0] *= t;
-    this->e[1] *= t;
-    this->e[2] *= t;
+inline vec3& vec3::operator+=(const vec3 &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
     return *this;
 }
 
-inline vec3 vec3::operator/=(const float t){
-    // Multiplication is faster than division
-    float k = 1.0f / t;
-    this->e[0] *= k;
-    this->e[1] *= k;
-    this->e[2] *= k;
+inline vec3& vec3::operator-=(const vec3& v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const float t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v) {
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const float t) {
+    float k = 1.0f/t;
+
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
     return *this;
 }
 
